6f8e5d5e71597ebe0ef53f41580f12f4c4eae01c
Dmitry Yadykov <dmitry.yadykov@gmail.com>
2020-12-11 19:09:03 +0300

    commit review

--- app/frontend/views/main/index.php
+++ app/frontend/views/main/index.php
@@ -1,28 +1,7 @@
 <?php
 
-
-use yii\data\ActiveDataProvider;
-use yii\grid\GridView;
-
 /* @var $this yii\web\View */
 $this->title = 'Цветы в Чебоксарах';
-
-/* @var $activeDataProvider ActiveDataProvider */
-
-echo GridView::widget([
-    'dataProvider' => $activeDataProvider,
-    'columns' => [
-        [
-            'attribute' => 'id',
-            'value' => function ($model) {
-                return $model->id * 25;
-            }
-        ],
-        'username',
-        'authKey',
-        'email',
-    ],
-]);
 
 ?>
 Главная страница

a1d9607bc9bf4b0e36ab3550df0a670a22a8ba69
Dmitry Yadykov <dmitry.yadykov@gmail.com>
2020-12-18 19:35:52 +0300

    CV-3 - CR-fix

--- app/frontend/models/Slide.php
+++ app/frontend/models/Slide.php
@@ -27,7 +27,6 @@ class Slide extends ActiveRecord
 
     public function rules()
     {
-        // TODO 3 изучить rules в документации движка в интернете
         return [
             [['id', 'pos'], 'integer', 'message' => 'Поле "{attribute}" должно быть числом'],
             [['src', 'url', 'alt'], 'string'],

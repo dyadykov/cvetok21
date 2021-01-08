5f5e54e24ec8c2e292ef39f0fb7e87af78d4568a
Dmitry Yadykov <dmitry.yadykov@gmail.com>
2021-01-08 17:44:52 +0300

    CV-8 - Добавление фильтра по продуктам в каталоге

--- app/frontend/controllers/CatalogController.php
+++ app/frontend/controllers/CatalogController.php
@@ -4,6 +4,8 @@
 namespace frontend\controllers;
 
 use frontend\models\Product;
+use frontend\models\SearchForm;
+use Yii;
 use yii\data\Pagination;
 
 class CatalogController extends CommonController
@@ -12,10 +14,27 @@ class CatalogController extends CommonController
 
     public function actionIndex()
     {
-        $activeQuery = Product::find()->where(['isActive' => 1]);
+        $activeQuery = Product::find()->where(['isActive' => 1])->orderBy('id');
+
+        $searchForm = new SearchForm();
+        $searchData = Yii::$app->request->post();
+        if ($searchForm->load($searchData) && $searchForm->validate()) {
+            $activeQuery
+                ->andFilterWhere(['>=', 'price', $searchForm->priceMin])
+                ->andFilterWhere(['<=', 'price', $searchForm->priceMax])
+                ->andFilterWhere(['isFavourite' => $searchForm->isFavourite ?: null])
+                ->andFilterWhere(['title' => $searchForm->title])
+                ->andFilterWhere(['like', 'description', $searchForm->description])
+;
+        } elseif (!empty($searchData)) {
+            Yii::$app->session->setFlash('error', "Фильтр не сработал");
+        }
+
         $cloneActiveQuery = clone $activeQuery;
+
         $pagination = new Pagination(['totalCount' => $cloneActiveQuery->count()]);
         $pagination->setPageSize(self::PAGE_SIZE);
+
         $models = $activeQuery
             ->offset($pagination->offset)
             ->limit($pagination->limit)
@@ -24,6 +43,7 @@ class CatalogController extends CommonController
         return $this->render('index', [
             'models' => $models,
             'pagination' => $pagination,
+            'searchForm' => $searchForm,
         ]);
     }
 }
